53cd819fa04b136e2916ee22416e87a5290081ca
Aryan <aryanxofficial@gmail.com>
2023-08-25 16:41:57 +0530

    filters added

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,17 +1,46 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+//I am changing this stateless widget to a stateful widget because I want to know the filter that has been clicked on and that will change the state of the selectedFilter variable
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<String> filters = const [
+    'All',
+    'Adidas',
+    'Reebok',
+    'Nike',
+    'Puma',
+    'Onitsuka Tiger',
+    'Jordan',
+    'Gucci'
+  ];
+
+  //In flutter I cannot use an initialized value to initialize another value,
+  //thats why I use late keyword and then initialize that variable inside the init method
+  // String selectedFilter = filters[0];
+  late String selectedFilter;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    selectedFilter = filters[0];
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
-              Row(
+              const Row(
                 children: [
                   Text(
                     'Shoes \nCollection',
@@ -23,13 +52,42 @@ class HomePage extends StatelessWidget {
                   Expanded(
                     child: TextField(
                       decoration: InputDecoration(
+                        // hoverColor: Colors.red,
                         prefixIcon: Icon(Icons.search),
                         hintText: 'Search',
                         hintStyle: TextStyle(fontWeight: FontWeight.bold),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey),
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                        ),
                       ),
                     ),
                   )
                 ],
+              ),
+              SizedBox(
+                height: 120,
+                child: ListView.builder(
+                    itemCount: filters.length,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      final filter = filters[index];
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: GestureDetector(
+                          onTap: () {
+                            selectedFilter = filter;
+                            print(selectedFilter);
+                          },
+                          child: Chip(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 15),
+                            label: Text(filter),
+                            labelStyle: const TextStyle(color: Colors.black),
+                          ),
+                        ),
+                      );
+                    }),
               )
             ],
           ),
--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: 'Lato',
+      theme: ThemeData(
+      fontFamily: 'Lato',
       colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow)),//by mentioning the font family here I dont have to include my custom font everytime in the TextStyle property of a text
       title: 'Shopping App',
       home: const HomePage(),
