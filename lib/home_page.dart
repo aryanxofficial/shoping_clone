857108ef8704e596828e2e8c58102b0fab71bd47
Aryan <aryanxofficial@gmail.com>
2023-08-31 03:08:40 +0530

    bottom navigation added

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/cart_page.dart';
 import 'package:shop_app/global_variables.dart';
 import 'package:shop_app/product_card.dart';
 import 'package:shop_app/product_detail_page.dart';
+import 'package:shop_app/product_list.dart';
 
 //I am changing this stateless widget to a stateful widget because I want to know the filter that has been clicked on and that will change the state of the selectedFilter variable
 class HomePage extends StatefulWidget {
@@ -12,120 +14,32 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final List<String> filters = const [
-    'All',
-    'Adidas',
-    'Reebok',
-    'Nike',
-    'Puma',
-    'Onitsuka Tiger',
-    'Jordan',
-    'Gucci'
-  ];
-
   //In flutter I cannot use an initialized value to initialize another value,
   //thats why I use late keyword and then initialize that variable inside the init method
   // String selectedFilter = filters[0];
-  late String selectedFilter;
+  int currentPage = 0;
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    selectedFilter = filters[0];
-  }
+  List<Widget> pages = const [ProductList(), CartPage()];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              const Row(
-                children: [
-                  Text(
-                    'Shoes \nCollection',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
-                  ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        // hoverColor: Colors.red,
-                        prefixIcon: Icon(Icons.search),
-                        hintText: 'Search',
-                        hintStyle: TextStyle(fontWeight: FontWeight.bold),
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey),
-                          borderRadius: BorderRadius.all(Radius.circular(20)),
-                        ),
-                      ),
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 120,
-                child: ListView.builder(
-                    itemCount: filters.length,
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (context, index) {
-                      final filter = filters[index];
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: GestureDetector(
-                          onTap: () {
-                            setState(() {});
-                            selectedFilter = filter;
-                            print(selectedFilter);
-                          },
-                          child: Chip(
-                            backgroundColor: selectedFilter == filter
-                                // ? Color.fromARGB(255, 252, 228, 12)
-                                ? Theme.of(context)
-                                    .colorScheme
-                                    .primary // from this line I can basically inherit the properties of the parent widget. I am using the colorscheme that is defined inside the colorscheme of the themeData inside my material App.
-                                : const Color.fromARGB(255, 241, 239, 239),
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 10, vertical: 15),
-                            label: Text(filter),
-                            labelStyle: const TextStyle(color: Colors.black),
-                          ),
-                        ),
-                      );
-                    }),
-              ),
-              Expanded(
-                child: ListView.builder(
-                    itemCount: products.length,
-                    itemBuilder: (context, index) {
-                      final product = products[index];
-                      return GestureDetector(
-                        onTap: () {
-                          Navigator.of(context)
-                              .push(MaterialPageRoute(builder: (context) {
-                            return ProductDetailPage(product: product);
-                          }));
-                        },
-                        child: productCard(
-                          title: product['title'] as String,
-                          price: product['price'] as String,
-                          image: product['imageURL'] as String,
-                          backgroundColor: index.isEven
-                              ? Color.fromARGB(255, 137, 204, 235)
-                              : const Color.fromARGB(255, 241, 239, 239),
-                        ),
-                      );
-                    }),
-              )
-            ],
-          ),
-        ),
-      ),
+      body: pages[currentPage],
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: (value) {
+            setState(() {
+              currentPage = value;
+              //No need of navigator since in body only I can pass variable as hrr bar bottom navigation wla dabane pr widget to waise bhi poora rebuild ho hi raha hai
+              // Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+              //   return pages[value];
+              // }));
+            });
+          },
+          currentIndex: currentPage,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+            BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: '')
+          ]),
     );
   }
 }
---
+++ lib/product_list.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/home_page.dart';
+import 'package:shop_app/global_variables.dart';
+import 'package:shop_app/product_card.dart';
+import 'package:shop_app/product_detail_page.dart';
+
+class ProductList extends StatefulWidget {
+  const ProductList({super.key});
+
+  @override
+  State<ProductList> createState() => _ProductListState();
+}
+
+class _ProductListState extends State<ProductList> {
+  final List<String> filters = const [
+    'All',
+    'Adidas',
+    'Reebok',
+    'Nike',
+    'Puma',
+    'Onitsuka Tiger',
+    'Jordan',
+    'Gucci'
+  ];
+
+  late String selectedFilter;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    selectedFilter = filters[0];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const Row(
+                children: [
+                  Text(
+                    'Shoes \nCollection',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        // hoverColor: Colors.red,
+                        prefixIcon: Icon(Icons.search),
+                        hintText: 'Search',
+                        hintStyle: TextStyle(fontWeight: FontWeight.bold),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey),
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 120,
+                child: ListView.builder(
+                    itemCount: filters.length,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      final filter = filters[index];
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: GestureDetector(
+                          onTap: () {
+                            setState(() {});
+                            selectedFilter = filter;
+                            print(selectedFilter);
+                          },
+                          child: Chip(
+                            backgroundColor: selectedFilter == filter
+                                // ? Color.fromARGB(255, 252, 228, 12)
+                                ? Theme.of(context)
+                                    .colorScheme
+                                    .primary // from this line I can basically inherit the properties of the parent widget. I am using the colorscheme that is defined inside the colorscheme of the themeData inside my material App.
+                                : const Color.fromARGB(255, 241, 239, 239),
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 15),
+                            label: Text(filter),
+                            labelStyle: const TextStyle(color: Colors.black),
+                          ),
+                        ),
+                      );
+                    }),
+              ),
+              Expanded(
+                child: ListView.builder(
+                    itemCount: products.length,
+                    itemBuilder: (context, index) {
+                      final product = products[index];
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.of(context)
+                              .push(MaterialPageRoute(builder: (context) {
+                            return ProductDetailPage(product: product);
+                          }));
+                        },
+                        child: productCard(
+                          title: product['title'] as String,
+                          price: product['price'] as String,
+                          image: product['imageURL'] as String,
+                          backgroundColor: index.isEven
+                              ? Color.fromARGB(255, 137, 204, 235)
+                              : const Color.fromARGB(255, 241, 239, 239),
+                        ),
+                      );
+                    }),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
